31276e4f383a75fcc32dbb5d64f96fdd4016f533
Nguyen Dinh Phi <phind.uet@gmail.com>
2025-09-23 13:31:29 +0800

    Detect arch in running
    Signed-off-by: Nguyen Dinh Phi <phind.uet@gmail.com>

--- running.sh
+++ running.sh
@@ -1,1 +1,8 @@
- ./tools/testing/kunit/kunit.py run --kunitconfig=mm/.kunitconfig --arch=x86_64 --kernel_args=movablecore=256M #--qemu_args="-s -S"
+#!/bin/bash 
+if [ "$(uname -m)" != "x86_64" ]; then
+    	echo "This system is NOT x86_64, need cross compile"
+	./tools/testing/kunit/kunit.py run --kunitconfig=mm/.kunitconfig --arch=x86_64 --kernel_args=movablecore=256M --cross_compile=x86_64-linux-gnu-
+else
+	echo "This system is x86_64"
+	./tools/testing/kunit/kunit.py run --kunitconfig=mm/.kunitconfig --arch=x86_64 --kernel_args=movablecore=256M
+fi
